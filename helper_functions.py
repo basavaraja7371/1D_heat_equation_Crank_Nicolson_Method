e2b2febc1f39dbac600ea58f621c0531a25c2d4b
Basavaraja G <basava@inchara.local>
2025-12-14 19:15:52 +0530

    Added boundary vector function

--- helper_functions.py
+++ helper_functions.py
@@ -29,8 +29,8 @@ def d2_dirichlet(nx, dx):
 
 def crank_nicolson_matrices(nx, dx, dt, alpha):
     """
-    Create matrices involved in Crank-Nicolson method. (I-A) on the LHS,
-    (I+A) on the RHS and vector r.
+    Create matrices involved in Crank-Nicolson method. (I-A) on the LHS, (I+A)
+    on the RHS and vector r.
 
     Args:
         nx (integer): number of grid points
@@ -54,3 +54,26 @@ def crank_nicolson_matrices(nx, dx, dt, alpha):
     RHS = Ix + A
 
     return LHS, RHS, r
+
+
+def boundary_vector(r, g0_n, g0_np1, gL_n, gL_np1, nx):
+    """
+    Create the boundary vector constituting of boundary condition for
+    Crank-Nicolson method.
+
+    Args:
+        r (np.array): r vector dt * alpha / dx**2
+        g0_n (float): Left boundary condtion at time t[n]
+        g0_np1 (float): Left boundary condition at time t[n+1]
+        gL_n (float): Right boundary condtion at time t[n]
+        gL_np1 (float): Right boundary condtion at time t[n+1]
+        nx (float): number of grid points
+
+    Returns:
+        b (np.array): boundary vector
+    """
+    b = np.zeros(nx - 2)
+    b[0] = (r / 2) * (g0_n + g0_np1)
+    b[-1] = (r / 2) * (gL_n + gL_np1)
+
+    return b
